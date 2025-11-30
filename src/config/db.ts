c6e3f226e2591aa5c33a2f668c29f34dfd3658db
Andrés Darín <andres.darin05@gmail.com>
2025-11-30 09:15:46 -0300

    feat: Enhance logging with colors for MongoDB connection and server startup

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
+import colors from 'colors';
 
 export const connectDB = async () => {
     try {
         const URL_DB = process.env.MONGODB_URI || 'mongodb://localhost:27017/devtree';
         const conection = await mongoose.connect(URL_DB);
-        const url = `${conection.connection.host}:${conection.connection.port}`;
-        console.log("MongoDB connected in:", url);
+        const url = `${colors.magenta.bold(conection.connection.host)}:${colors.magenta.bold(conection.connection.port.toString())}`;
+        console.log(colors.green.italic("MongoDB connected in:"), url);
 
 
     } catch (error) {
-        console.error("Error connecting to the database", error);
+        console.error(colors.bgRed.white.bold("Error connecting to the database"), error);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import colors from "colors";
 import server from "./server";
 
 //Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+    console.log(colors.cyan.italic(`Servidor escuchando en el puerto ${PORT}`));
 });
 
